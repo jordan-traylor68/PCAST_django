c8d46d6373c9bb50b9ccd73c3e4f30ef21ff04ab
devinvonarx <59193862+devinvonarx@users.noreply.github.com>
2023-09-22 17:13:53 -0500

    create document class

--- PCAST_django/documents/admin.py
+++ PCAST_django/documents/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from documents.models import *
 # Register your models here.
+
+class DocumentAdmin(admin.ModelAdmin):
+    readonly_fields=['quartex_uid','quartex_name','asset_id']
+    list_display=['title','lang','asset_id']
+    search_fields=['title','asset_id']
+    autocomplete_fields=['subjects']
+
+class LegacySubjectsAdmin(admin.ModelAdmin):
+    search_fields=['name']
+    list_display=['name']
+    readonly_display=['name']
+
+admin.site.register(Document,DocumentAdmin)
+admin.site.register(LegacySubjects,LegacySubjectsAdmin)
--- PCAST_django/documents/models.py
+++ PCAST_django/documents/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Document(models.Model):
+    title=models.CharField(max_length=500,null=False,blank=False)
+    asset_id=models.IntegerField(null=False,blank=False,unique=True)
+    quartex_uid=models.CharField(max_length=36,null=False,blank=False,unique=True)
+    quartex_name=models.CharField(max_length=100,null=False,blank=False,unique=True)
+    lang=models.CharField(max_length=3,null=True,blank=True,unique=False)
+    subjects=models.ManyToManyField('LegacySubjects')
+    def __str__(self):
+        return self.title
+
+class LegacySubjects(models.Model):
+    name=models.CharField(max_length=500,null=False,blank=False)
+    def __str__(self):
+        return self.name
