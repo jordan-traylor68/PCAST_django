11b4c83058770e3fc2feccdd85c4c225690849ed
Jordan Traylor <jt68@rice.edu>
2024-01-10 13:57:19 -0600

    * to allowed hosts

--- PCAST_django/PCAST_django/settings.py
+++ PCAST_django/PCAST_django/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-3t6pbc(l$*s1%!7cofa0u*^1!)uzprn8^ztv4j#8g$_e50s6nw
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
