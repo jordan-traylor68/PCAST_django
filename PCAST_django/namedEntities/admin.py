437ba80303c8be0005ca174cdcd7004967b5e27b
Jordan Traylor <jt68@rice.edu>
2024-02-01 11:28:24 -0600

    added sectors and an action on admin view to edit

--- PCAST_django/namedEntities/admin.py
+++ PCAST_django/namedEntities/admin.py
@@ -3,7 +3,48 @@ from namedEntities.models import *
 
 # Register your models here.
 
+def set_sector_fg(ModelAdmin, request, queryset):
+	sector = Sector.objects.get(name='Federal government')
+	for Institution in queryset:
+		Institution.sector = sector
+		Institution.save()
 
+def set_sector_sg(ModelAdmin, request, queryset):
+	sector = Sector.objects.get(name='State government')
+	for Institution in queryset:
+		Institution.sector = sector
+		Institution.save()
+
+def set_sector_lg(ModelAdmin, request, queryset):
+	sector = Sector.objects.get(name='Local government')
+	for Institution in queryset:
+		Institution.sector = sector
+		Institution.save()
+
+def set_sector_ps(ModelAdmin, request, queryset):
+	sector = Sector.objects.get(name='Private sector')
+	for Institution in queryset:
+		Institution.sector = sector
+		Institution.save()
+
+def set_sector_ns(ModelAdmin, request, queryset):
+	sector = Sector.objects.get(name='Nonprofit sector')
+	for Institution in queryset:
+		Institution.sector = sector
+		Institution.save()
+
+def set_sector_ac(ModelAdmin, request, queryset):
+	sector = Sector.objects.get(name='Academia')
+	for Institution in queryset:
+		Institution.sector = sector
+		Institution.save()
+
+set_sector_fg.short_description = "Set sector to Federal government"
+set_sector_sg.short_description = "Set sector to State government"
+set_sector_lg.short_description = "Set sector to Local government"
+set_sector_ps.short_description = "Set sector to Private sector"
+set_sector_ns.short_description = "Set sector to Nonprofit sector"
+set_sector_ac.short_description = "Set sector to Academia"
 
 class PersonAdmin(admin.ModelAdmin):
 	list_display=['name','birth_year','death_year','wikipedia_grade']
@@ -11,9 +52,10 @@ class PersonAdmin(admin.ModelAdmin):
 
 class InstitutionAdmin(admin.ModelAdmin):
 	list_display=['name','sector']
+	actions = [set_sector_fg,set_sector_ac,set_sector_lg,set_sector_ns,set_sector_ps,set_sector_sg]
 
 admin.site.register(Person,PersonAdmin)
-admin.site.register(Institution)
+admin.site.register(Institution,InstitutionAdmin)
 admin.site.register(Degree)
 admin.site.register(Country)
 admin.site.register(Ethnicity)
--- PCAST_django/namedEntities/models.py
+++ PCAST_django/namedEntities/models.py
@@ -2,6 +2,15 @@ from django.db import models
 from documents.models import Document
 # Create your models here.
 
+# STATUS_CHOICES = {
+#     "fg": "Federal government"
+# 	"sg": "State government"
+# 	"lg": "Local government"
+# 	"ps": "Private sector"
+# 	"ns": "Nonprofit sector"
+# 	"ac": "Academia"
+# }
+
 class Ethnicity(models.Model):
 	controlled_name=models.CharField(
 		max_length=255,
@@ -73,7 +82,6 @@ class Institution(models.Model):
 		blank=True,
 		on_delete=models.SET_NULL
 	)
-
 	def __str__(self):
 		return self.name
 
