7535d93d8c201ba5947121e2ec0c9b571557f29b
Devin Von Arx <dav8@rice.edu>
2024-02-05 14:02:32 -0600

    added space to named entities

--- PCAST_django/namedEntities/apps.py
+++ PCAST_django/namedEntities/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class NamedentitiesConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "namedEntities"
+    verbose_name = "Named Entities"
