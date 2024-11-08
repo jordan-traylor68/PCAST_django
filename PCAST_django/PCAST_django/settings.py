951dca2f8d8350b4084c4ed6eebd3418f021291f
Jordan Traylor <jt68@rice.edu>
2024-11-08 15:51:54 -0600

    testing postgres database setup

--- PCAST_django/PCAST_django/settings.py
+++ PCAST_django/PCAST_django/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     "documents",
     "namedEntities",
     'nested_admin',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -80,8 +79,12 @@ WSGI_APPLICATION = 'PCAST_django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pg_db',
+        'USER': 'jt68',
+        'PASSWORD': 'green',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
