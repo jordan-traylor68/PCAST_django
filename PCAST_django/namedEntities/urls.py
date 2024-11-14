3ba3fbbbb8bc1f1fd18b17ace04d042719919719
Yujie Xu <yx77@rice.edu>
2024-11-14 12:01:16 -0600

    person institution api

--- PCAST_django/namedEntities/urls.py
+++ PCAST_django/namedEntities/urls.py
@@ -1,7 +1,14 @@
-from django.urls import path
-from . import views_api
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views_api import PersonViewSet, InstitutionViewSet
+
+router = DefaultRouter()
+
+router.register(r'persons', PersonViewSet)
+router.register(r'institution', InstitutionViewSet)
+
 
 urlpatterns = [
-
-    path('api/person/<int:pk>/', views_api.PersonDetailAPIView.as_view(), name='api-person-detail'),
+    path('api/', include(router.urls)),
+    path('api/', include(router.urls)),
 ]
