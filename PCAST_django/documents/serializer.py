8fa784c8aeca80933b30e6ef755d76602d309f92
Jordan Traylor <jt68@rice.edu>
2025-01-30 13:59:50 -0600

    removed id from docs serializer

--- PCAST_django/documents/serializer.py
+++ PCAST_django/documents/serializer.py
@@ -8,7 +8,7 @@ from rest_framework import serializers
 from .models import Document
 
 class Docs_Serializer(serializers.ModelSerializer):
-    id = serializers.IntegerField()
+    # id = serializers.IntegerField()
     class Meta:
         model = Document
         fields = '__all__'
